03cf51ac4bb205adcc5f7d663165095e047d32d6
Matthias Bigl <biglmatthias@gmail.com>
2023-04-19 17:59:10 +0200

    made styling changes

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   publicProcedure,
-  protectedProcedure,
 } from "~/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
@@ -16,7 +15,4 @@ export const exampleRouter = createTRPCRouter({
     }),
   
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
 });
