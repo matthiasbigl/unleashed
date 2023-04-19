0e9d35f120abced83c7e4fee9bee9386a12c43df
Matthias Bigl <biglmatthias@gmail.com>
2023-04-19 17:43:32 +0200

    changed to clerk and made styling changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 export default function Navbar() {
+
+  const user = useUser();
+  console.log(user);
+
     return (
         <nav
             className="flex z-10 flex-row justify-between items-center  h-16 border-b border-neutral-800 bg-gradient-to-b bg-zinc-800/30 ">
@@ -11,7 +16,8 @@ export default function Navbar() {
             </div>
             <div className="flex flex-row items-center justify-end w-1/2 h-full px-6 gap-x-5">
 
-                <div
+              {/*
+              <div
                     className={`flex flex-row items-center justify-center rounded-md border border-neutral-800 bg-zinc-800/30 w-24 h-10
                 hover:scale-105 hover:shadow-lg hover:bg-zinc-800/50 transition duration-150 ease-in-out
                 cursor-pointer
@@ -19,17 +25,49 @@ export default function Navbar() {
                     }>
                     <a href="/register" className="text-lg font-semibold ">Register</a>
                 </div>
-                <Link href={'/api/auth/signin'}
+
+              */}{
+              !user.isSignedIn ? (
+                <div
                     className={
                         `flex flex-row items-center justify-center rounded-md bg-gradient-to-r from-blue-400 to-blue-600 w-24 h-10
                     hover:scale-105 hover:shadow-lg hover:from-blue-500 hover:to-blue-700 transition duration-150 ease-in-out 
                     cursor-pointer
-                    font-semibold text-lg
+                    font-semibold text-lg 
                     `
                     }>
+                  <SignInButton/>
+                </div>) : (
+                <div className="
+                flex  flex-row items-center justify-end w-full h-full gap-x-4">
+                  <Link className='flex flex-row items-center-center gap-2 text-lg py-1 px-2  rounded-md
+                  hover:scale-105 hover:shadow-lg hover:bg-zinc-800/50 transition duration-150 ease-in-out' href={"/profile"}>
+                      <h2 className="my-auto">
+                        {user.user.fullName}
+                      </h2>
+                    <div
+                      //a circle avatar
+                      className={`rounded-full border border-neutral-800 bg-zinc-800/30 
+                  
+                    cursor-pointer
+                    `}
+                    >
+                      <img src={user.user.profileImageUrl} alt="avatar" className="rounded-full w-10 h-10"/>
 
-                     Login
-                </Link>
+                    </div>
+                  </Link>
+
+                  <div
+                    className={`flex flex-row items-center justify-center rounded-md border border-neutral-800 bg-zinc-800/30 w-24 h-10
+                hover:scale-105 hover:shadow-lg hover:bg-zinc-800/50 transition duration-150 ease-in-out
+                cursor-pointer
+                text-lg font-semibold 
+                `}>
+                    <SignOutButton/>
+                  </div>
+                </div>
+
+               )}
 
             </div>
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,24 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.scss";
 import Layout from "~/components/Layout";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType<{ session: Session | null }> = ({
                                                        Component,
                                                        pageProps: { session, ...pageProps }
                                                      }) => {
   return (
-    <SessionProvider session={session}>
+   <ClerkProvider {...pageProps}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </SessionProvider>
+    </ClerkProvider>
+
   );
 };
 
